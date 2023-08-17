a2e9cbf06f50d11170c6f7fe9c17319d8d7c7219
cfuntila <cfuntila@ucsc.edu>
2023-08-17 12:31:47 -0700

    Create character and Spell Models

--- HarryPotter/Models/HPCharacter.swift
+++ HarryPotter/Models/HPCharacter.swift
@@ -7,6 +7,38 @@
 
 import Foundation
 
-struct HPCharacter {
+struct HPCharacter: Codable {
+    let id: String
+    let type: String
+    let attributes: HPCharacterAttributes
+}
+
+struct HPCharacterAttributes: Codable {
+    let slug: String
+    let name: String
+    let born: String?
+    let died: Bool?
+    let gender: String?
+    let species: String?
+    let height: String?
+    let weight: String?
+    let hair_color: String?
+    let eye_color: String?
+    let skin_color: String?
+    let blood_status: String?
+    let marital_status: String?
+    let nationality: String?
+    let animagus: String?
+    let boggart: String?
+    let house: String?
+    let patronus: String?
     
+    let alias_names: [String]
+    let family_members: [String]
+    let jobs: [String]
+    let romances: [String]
+    let titles: [String]
+    let wands: [String]
+    let image: String?
+    let wiki: String?
 }
--- HarryPotter/Models/HPSpell.swift
+++ HarryPotter/Models/HPSpell.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
-struct HPSpell {
-    
+struct HPSpell: Codable {
+    let id: String
+    let type: String
+    let attributes: HPSpellAttributes
+}
+
+struct HPSpellAttributes: Codable {
+    let slug: String
+    let name: String
+    let incantation: String?
+    let category: String?
+    let effect: String?
+    let light: String?
+    let hand: String?
+    let creator:String?
+    let image: String?
+    let wiki: String?
 }
