a15c40db7a85f824ffc5107871abacd6ddf88df9
cfuntila <cfuntila@ucsc.edu>
2023-08-17 10:58:44 -0700

    Initial Commit

---
+++ HarryPotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotter
+//
+//  Created by Charity Funtila on 8/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
